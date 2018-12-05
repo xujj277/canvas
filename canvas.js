f3ec74803586c3791ee03e183e7d228ba0119b04
xujj277 <939160741@qq.com>
2018-12-05 16:13:46 +0800

    real use canvas to draw

--- canvas.js
+++ canvas.js
@@ -1,5 +1,26 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
+var painting = false
 
-context.fillStyle = 'red'
-context.fillRect(0, 0, 100, 100)
+function drawCircle (x, y, radius) {
+  context.beginPath()
+  context.arc(x, y, radius, 0, 360)
+  context.stroke()
+}
+
+canvas.onmousedown = function (val) {
+  painting = true
+  var x = val.clientX
+  var y = val.clientY
+  drawCircle(x, y, 1)
+}
+canvas.onmousemove = function (val) {
+  if (painting) {
+    var x = val.clientX
+    var y = val.clientY
+    drawCircle(x, y, 1)
+  }
+} 
+canvas.onmouseup = function (val) {
+  painting = false
+}
