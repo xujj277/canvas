8dfa35e1d1490966863bb9240e46d1b6a1fa7965
xujj277 <939160741@qq.com>
2018-12-06 11:14:24 +0800

    my first word

--- canvas.js
+++ canvas.js
@@ -1,67 +1,71 @@
-var canvas = document.getElementById('canvas')
-var context = canvas.getContext('2d')
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
 var using = false
 var lastPoint = {x: undefined, y: undefined}
-
 var eraserEnabled = false
-eraser.onclick = function () {
+
+autoResizeCanvas()
+listenToUser(canvas)
+
+
+eraser.onclick = function (val) {
   eraserEnabled = true
   actions.className = 'actions showBrushButton'
 }
-brush.onclick = function () {
+brush.onclick = function (val) {
   eraserEnabled = false
   actions.className = 'actions'
 }
-
-autoSetCanvasSize(canvas)
-listenToMouse(canvas)
-
-function drawLine (x1, y1, x2, y2) {
-  context.beginPath()
-  context.moveTo(x1, y1)   // 起点
-  context.lineWidth = 5
-  context.lineTo(x2, y2)   // 终点
-  context.stroke()
-  context.closePath()
-}
-
-function autoSetCanvasSize (canvas) {
+ 
+function autoResizeCanvas () {
   setCanvasSize()
   window.onresize = function () {
     setCanvasSize()
   }
-  function setCanvasSize () {
-    var pageWidth = document.documentElement.clientWidth
-    var pageHeight = document.documentElement.clientHeight
-    canvas.width = pageWidth
-    canvas.height = pageHeight
-  }
 }
 
-function listenToMouse (canvas) {
-  canvas.onmousedown = function (val) { 
+function setCanvasSize () {
+  var pageWidth = document.documentElement.clientWidth
+  var pageHeight = document.documentElement.clientHeight
+  canvas.width = pageWidth
+  canvas.height = pageHeight
+}
+function listenToUser (canvas) {
+  canvas.onmousedown = function (val) {
+    using = true
     var x = val.clientX
     var y = val.clientY
-    using = true
     if (eraserEnabled) {
       context.clearRect(x-5, y-5, 10, 10)
     } else {
       lastPoint = {x: x, y: y}
     }
   }
+  
   canvas.onmousemove = function (val) {
     var x = val.clientX
     var y = val.clientY
+    var newPoint = {x: x, y: y}
     if (!using) return
     if (eraserEnabled) {
       context.clearRect(x-5, y-5, 10, 10)
     } else {
-      var newPoint = {x: x, y: y}
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      drawLines(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
       lastPoint = newPoint
     }
-  } 
+  }
+  
   canvas.onmouseup = function (val) {
     using = false
   }
 }
+
+function drawLines (x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineWidth = 5;
+  context.lineTo(x2, y2);
+  context.closePath();
+  context.stroke();
+}
+
